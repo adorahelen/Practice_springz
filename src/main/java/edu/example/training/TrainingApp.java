2af2480c689394f524a87f0580966a87705ab8e8
adorahelen <adorahelenmin@gmail.com>
2024-08-08 22:20:01 +0900

    property

--- src/main/java/edu/example/training/TrainingApp.java
+++ src/main/java/edu/example/training/TrainingApp.java
@@ -6,15 +6,36 @@ import edu.example.training.repository.MockTrainingRepository;
 import edu.example.training.repository.TrainingRepository;
 import edu.example.training.service.TrainingService;
 import edu.example.training.service.TrainingServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
+@Configuration // 저절로 자바 컨피그 클래스가 되어
+@ComponentScan // 현재 패키지 부터 하위 모든 패키지를 포함하여 스테레오타입 어노테이션 클래스의 객체를 찾아 빈으로 관리
+// @ComponentScan("com.example.traing") // 경로를 지정하여 컴포넌트 스캔
 public class TrainingApp {
     public static void main(String[] args) {
-        TrainingRepository tr = new JdbcTrainingRepository();
-                            tr = new MockTrainingRepository();
-                            // 이런 식으로 객체 생성 후 넘기는 작업을 빈이라고 하고, 이걸 스프링이 대신 진행하게 될 것이다. DI
-        TrainingService ts = new TrainingServiceImpl(tr);
+        //TrainingRepository tr = new JdbcTrainingRepository();
+          //                  tr = new MockTrainingRepository();
+        // 이런 식으로 객체 생성 후 넘기는 작업을 빈이라고 하고, 이걸 스프링이 대신 진행하게 될 것이다. DI
+
+       // TrainingService ts = new TrainingServiceImpl(tr);
+       // List<Training> trainings = ts.findAll();
+
+        // 시스템 환경변수를 이용하여 Profile 지정
+        // 이론상 위에 레포지토리 어노테이션이 기존의 것과 중복되기에 에러가 뜨지만
+        // JVM 환경설정안에 프로파일 지정으로 해결하거나
+        // OS 환경변수에 프로파일을 지정해서 해결 할 수 있다.
+        System.setProperty("spring.profiles.active", "production");
+
+        ApplicationContext context
+                = new AnnotationConfigApplicationContext(TrainingApp.class);
+
+        TrainingService ts = context.getBean(TrainingService.class);
         List<Training> trainings = ts.findAll();
+
     }
 }
