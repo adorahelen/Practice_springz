baf398831456c5a98afb4d743de9776b7d6391d7
adorahelen <adorahelenmin@gmail.com>
2024-08-13 17:49:51 +0900

    View / Controller : 시작

--- src/main/java/edu/example/sample/SampleApp.java
+++ src/main/java/edu/example/sample/SampleApp.java
@@ -1,8 +1,6 @@
 package edu.example.sample;
 
-import edu.example.training.entity.Reservation;
-import edu.example.training.input.ReservationInput;
-import edu.example.training.service.ReservationService;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
--- src/main/java/edu/example/sample/controller/SampleController.java
+++ src/main/java/edu/example/sample/controller/SampleController.java
@@ -2,12 +2,19 @@ package edu.example.sample.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SampleController {
     @GetMapping("/display-sample")
-    public String displaySample() {
-        return "sample";
+    public String displaySample(Model model ) {
+        // 뷰로 보낼 데이터는 모델 객체의 속성에 키와 값으로 추가
+        // 보낼 데이터가 없으면 안쓰지만, (매핑만 하지만), 있다면 사용
+        model.addAttribute("fullName", "Hello World!");
+        return "sample";                   // 키             값
     }
+    // 뷰 템플릿 파일 경로는 뷰리졸버가 스프링 부트 자동 설정을 따른다.
+    // 자동 설정된 경로 : /src/resources/templates/뷰이름.html
+    // [ prefix ]
 } // localhost:8080/display-sample
