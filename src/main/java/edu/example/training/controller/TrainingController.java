94276147b6c0fae8c3e2fffc9e51b921eb82a340
adorahelen <adorahelenmin@gmail.com>
2024-08-14 12:37:56 +0900

    View

--- src/main/java/edu/example/training/controller/TrainingController.java
+++ src/main/java/edu/example/training/controller/TrainingController.java
@@ -3,17 +3,26 @@ package edu.example.training.controller;
 import edu.example.training.service.TrainingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/training") // all mapping
 public class TrainingController {
-
-    @Autowired
     private final TrainingService trainingService;
 
-    @Autowired
+
     public TrainingController(TrainingService trainingService) {
         this.trainingService = trainingService;
         System.out.println("TrainingController created");
+    }
+
+    @GetMapping("/display-details")
+    public String displayDetails(Model model) {
+        System.out.println("TrainingController display-details");
+        return "training/trainingDetails";
+
     }
 
 }
