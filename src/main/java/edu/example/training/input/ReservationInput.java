c66375e5a4af1c13d0ac7dcad0c3f3939677cf17
adorahelen <adorahelenmin@gmail.com>
2024-08-14 16:13:43 +0900

    입력검사 규칙

--- src/main/java/edu/example/training/input/ReservationInput.java
+++ src/main/java/edu/example/training/input/ReservationInput.java
@@ -1,15 +1,28 @@
 package edu.example.training.input;
 // DTO
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 
 @Data
 public class ReservationInput {
     private String trainingId;
+
+    @NotBlank
     private String studentTypeCode;
+
+    @NotBlank
     private String name;
+
+    @NotBlank
+    @Pattern(regexp = "0\\d{1,4}-\\d{1,4}\\d{4}")
     private String phone;
+
+    @NotBlank
+    @Email
     private String emailAddress;
 
 }
