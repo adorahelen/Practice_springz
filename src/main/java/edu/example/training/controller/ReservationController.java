c6c786dc638c02df0c3b2a8da440078bb408a488
adorahelen <adorahelenmin@gmail.com>
2024-08-19 12:55:00 +0900

    프론트/백단 -> params로 분리

--- src/main/java/edu/example/training/controller/ReservationController.java
+++ src/main/java/edu/example/training/controller/ReservationController.java
@@ -1,8 +1,11 @@
 package edu.example.training.controller;
 
+import edu.example.training.entity.Reservation;
 import edu.example.training.entity.StudentType;
+import edu.example.training.entity.Training;
 import edu.example.training.input.ReservationInput;
 import edu.example.training.service.ReservationService;
+import edu.example.training.service.TrainingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,10 +20,13 @@ import java.util.List;
 public class ReservationController {
 
     private final ReservationService reservationService;
+    private final TrainingService trainingService;
 
     // 하나니까 안쓰고 저번에는 여로개로서 쓴거고 다 하기 싫으면 올알규스
-    public ReservationController(ReservationService reservationService) {
+    public ReservationController(ReservationService reservationService, TrainingService trainingService) {
+
         this.reservationService = reservationService;
+        this.trainingService = trainingService;
     }
 
     @GetMapping("/display-form")
@@ -49,11 +55,30 @@ public class ReservationController {
             model.addAttribute("studentTypeList", studentTypes);
             return "reservation/reservationForm";
         }
-
+        // 입력오류가 없을 경우 해당 트래이닝 정보를 가지고 화면으로 이동
+        Training training = trainingService.findById(reservationInput.getTrainingId());
+        model.addAttribute("training", training);
         return "reservation/reservationConfirm";
 
     }
 
+    @PostMapping(value = "/reserve", params = "reserve") // 신청확정
+    public String reserve(@Validated ReservationInput reservationInput,
+                          Model model){
+        // reservation 테이블에 추가하고
+        Reservation reservation = reservationService.reserve(reservationInput);
+        model.addAttribute("reservation", reservation);
+
+        return "reservation/reservationCompleted";
+    }
+
+    @PostMapping(value = "/reserve", params = "correct") // 신청 내용 변경
+    public String correctInput(@Validated ReservationInput reservationInput,
+                                Model model) {
+        List<StudentType> studentTypes = reservationService.getStudentTypes();
+        model.addAttribute("studentTypeList", studentTypes);
+        return "reservation/reservationForm";
+    }
 
 
 }
