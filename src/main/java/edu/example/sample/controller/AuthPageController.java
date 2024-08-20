ae261f09663013f6e9e2b5df8194d2ed33ec5f97
adorahelen <adorahelenmin@gmail.com>
2024-08-20 16:02:58 +0900

    폼파일, 시큐리티 컨트롤러

---
+++ src/main/java/edu/example/sample/controller/AuthPageController.java
@@ -0,0 +1,25 @@
+package edu.example.sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthPageController {
+    @GetMapping("/login")
+    public String loginForm(){
+        return "auth/loginForm";
+    }
+
+    @GetMapping(value="/login", params = "failure")
+    public String loginFail(Model model){
+        model.addAttribute("failureMessage", "로그인 실패");
+        return "auth/loginForm";
+    }
+
+    @RequestMapping("/display-access-denied")
+    public String accessDenied(){
+        return "auth/accessDenied";
+    }
+}
