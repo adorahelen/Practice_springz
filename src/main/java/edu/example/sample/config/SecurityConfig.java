5d78d0e0fbaf42faf2d1889bc0c9d232637a0de7
adorahelen <adorahelenmin@gmail.com>
2024-08-20 21:31:59 +0900

    권한 부여 데이터 추가

--- src/main/java/edu/example/sample/config/SecurityConfig.java
+++ src/main/java/edu/example/sample/config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,9 +19,9 @@ public class SecurityConfig {
     public SecurityFilterChain
     securityFilterChain(HttpSecurity httpSSS)
             throws Exception {
-        httpSSS.authorizeHttpRequests( // 요청 관련 인증 설정
+        httpSSS.authorizeHttpRequests( // 요청 관련 인증 설정===========
                 req -> req.requestMatchers(HttpMethod.POST,
-                   "/display-sample/**")
+                                "/display-sample/**")
                         .hasRole("ADMIN")
                         .requestMatchers(
                                 "/display-sample/**"
@@ -25,14 +29,38 @@ public class SecurityConfig {
                                 "ADMIN", "STAFF"
                         )
                         .anyRequest().permitAll()
-        ).formLogin(login-> // 로그인 화면 설정
+        ).formLogin(login -> // 로그인 화면 설정 ==============
                 login.loginPage("/login")
                         .defaultSuccessUrl("/display-sample")
                         .failureUrl("/login?failure")
 
-        ).exceptionHandling(handling-> // 예외 - 접근 거부 설정
+        ).exceptionHandling(handling -> // 예외 - 접근 거부 설정==========
                 handling.accessDeniedPage("/display-access-denied"));
 
         return httpSSS.build();
     }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails hana = User.builder().username("hana")
+                .password("{noop}1111")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails dool = User.builder().username("dool")
+                .password("{noop}1111")
+                .roles("STAFF")
+                .build();
+
+        UserDetails saam = User.builder().username("saam")
+                .password("{noop}1111")
+                .roles("GUEST")
+                .build();
+// noop == 암호화 되지 않았음을 의미한다.
+    return new InMemoryUserDetailsManager(hana, dool, saam);
+    }
+
+
 }
--- src/main/java/edu/example/sample/controller/SampleController.java
+++ src/main/java/edu/example/sample/controller/SampleController.java
@@ -11,10 +11,11 @@ public class SampleController {
     public String displaySample(Model model ) {
         // 뷰로 보낼 데이터는 모델 객체의 속성에 키와 값으로 추가
         // 보낼 데이터가 없으면 안쓰지만, (매핑만 하지만), 있다면 사용
-        model.addAttribute("fullName", "Hello World!");
+        model.addAttribute("fullName", "hana");
         return "sample";                   // 키             값
     }
     // 뷰 템플릿 파일 경로는 뷰리졸버가 스프링 부트 자동 설정을 따른다.
     // 자동 설정된 경로 : /src/resources/templates/뷰이름.html
     // [ prefix ]
 } // localhost:8080/display-sample
+// ㅋㅋㅋㅋㅋ
