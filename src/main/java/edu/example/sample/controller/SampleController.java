986105d4515668b2e1d03656af88f29be94b5cf0
adorahelen <adorahelenmin@gmail.com>
2024-08-21 10:28:03 +0900

    저장된 시큐리티 정보 확인

--- src/main/java/edu/example/sample/controller/SampleController.java
+++ src/main/java/edu/example/sample/controller/SampleController.java
@@ -1,14 +1,31 @@
 package edu.example.sample.controller;
 
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class SampleController {
     @GetMapping("/display-sample")
-    public String displaySample(Model model ) {
+    public String displaySample(Model model,
+                                Authentication authentication,
+                                @AuthenticationPrincipal UserDetails userDetails ) {
+        System.out.println("user : " + authentication.getName());
+        System.out.println("principal : " + authentication.getPrincipal());
+        authentication.getAuthorities().forEach(System.out::println);
+
+        System.out.println("==UserDetails==");
+        System.out.println(userDetails);
+
+        System.out.println("==");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(auth);
         // 뷰로 보낼 데이터는 모델 객체의 속성에 키와 값으로 추가
         // 보낼 데이터가 없으면 안쓰지만, (매핑만 하지만), 있다면 사용
         model.addAttribute("fullName", "hana");
